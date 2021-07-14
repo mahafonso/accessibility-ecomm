7e12dfc0f68f3787ae0e65148a1e8bcde88bafd2
mahafonso <marcela.bafonso3@gmail.com>
2021-07-14 17:03:21 -0300

    feat: 🎸 return to p tag

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -1,6 +1,6 @@
 import t from 'prop-types'
 
-const Text = ({content, ...rest}) => <img alt={content}>{content}</img>
+const Text = ({content, ...rest}) => <p>{content}</p>
 
 Text.propTypes = {
 	content: t.string.isRequired,
