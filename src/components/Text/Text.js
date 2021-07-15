03de77bf15d385f6ec74af49eeb45ecb0e120bf7
mahafonso <marcela.bafonso3@gmail.com>
2021-07-15 16:54:45 -0300

    feat: 🎸 testing link accessibility

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -1,6 +1,6 @@
 import t from 'prop-types'
 
-const Text = ({content, ...rest}) => <p>{content}</p>
+const Text = ({content, ...rest}) => <a>{content}</a>
 
 Text.propTypes = {
 	content: t.string.isRequired,
